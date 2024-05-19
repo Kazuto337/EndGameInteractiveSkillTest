7ba0537b0fad72d02764cbc515ae0aaf3a0c5fd9
Kazuto337 <60271850+Kazuto337@users.noreply.github.com>
2024-05-18 20:32:52 -0500

    Animation system on script with layers

--- Assets/Scripts/PlayerBehaviour/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour/PlayerBehaviour.cs
@@ -6,16 +6,24 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     private PlayerActions playerInput;
+
+    [Header("Movement Properties")]
     private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
+    private Vector3 verticalVelocity;
+    private bool isGrounded;
     [SerializeField] private float playerSpeed = 2.0f;
     [SerializeField] private float gravityValue = -9.81f;
+
+    [Header("Animation Properties")]
+    private bool isMoving;
+    private bool isShooting;
+    private Animator animator;
 
     private void Awake()
     {
         playerInput = new PlayerActions();
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -25,14 +33,17 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Move()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        isGrounded = controller.isGrounded;
+        if (isGrounded && verticalVelocity.y < 0)
         {
-            playerVelocity.y = 0f;
+            verticalVelocity.y = 0f;
         }
 
         Vector2 movementVector = playerInput.PlayerMain.Move.ReadValue<Vector2>();
         Vector3 moveValue = new Vector3(movementVector.x, 0, movementVector.y);
+
+        isMoving = moveValue != Vector3.zero;
+
         controller.Move(moveValue * Time.deltaTime * playerSpeed);
 
         if (moveValue != Vector3.zero)
@@ -40,13 +51,36 @@ public class PlayerBehaviour : MonoBehaviour
             gameObject.transform.forward = moveValue;
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        verticalVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(verticalVelocity * Time.deltaTime);
+    }
+
+    private void Shoot()
+    {
+        bool isButtonPress = playerInput.PlayerMain.Fire.ReadValue<float>() > 0;
+
+        if (isButtonPress)
+        {
+            Debug.Log("Fire!");
+             isShooting = true;
+
+            return;
+        }
+
+        isShooting = false;
     }
 
     private void Update()
     {
         Move();
+        Shoot();
+        SetAnimationStates();
+    }
+
+    private void SetAnimationStates()
+    {
+        animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isShooting", isShooting);
     }
 
     private void OnDisable()
